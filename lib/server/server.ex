282c28c5c16cee948fafa0b6c3293ccb9e49c4da
Emilio <thecraftedgem@tuta.io>
2019-07-14 02:59:47 -0600

    wip

--- lib/server/server.ex
+++ lib/server/server.ex
@@ -1,6 +1,8 @@
 defmodule UrlShortener.Server do
+  alias UrlShortener.Url_Shortener.Url
+  alias UrlShortener.Repo
 
   def find_long_url(short_url) do
-    UrlShortener.Repo.get_by(UrlShortener.Url_Shortener.Url, short_address: short_url).long_address
+    Repo.get_by(Url, short_address: short_url).long_address
   end
 end
